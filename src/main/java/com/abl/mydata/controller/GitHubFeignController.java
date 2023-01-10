08ab69231b9f5fd02fbfd4e570146863a5b375ac
Byounggwan Lee <lbg@sk.com>
2023-01-10 12:45:02 +0900

    feign Client Sample 반영

--- src/main/java/com/abl/mydata/controller/GitHubFeignController.java
+++ src/main/java/com/abl/mydata/controller/GitHubFeignController.java
@@ -3,6 +3,8 @@ package com.abl.mydata.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +12,42 @@ import org.springframework.web.bind.annotation.RestController;
 import com.abl.mydata.domain.service.GitHubFeignService;
 import com.abl.mydata.dto.Contributor;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "feign Client통한 Git Hub Service 정보검색 Sample")
 public class GitHubFeignController {
 	@Autowired
 	private GitHubFeignService gitHubFeignService;
 
+	@Operation(summary = "Git Hub Owner와 Repository명으로 정보조회", description = "Git Hub 조회")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found the hcallifhists", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Contributor.class)) }),
+			 @ApiResponse(responseCode = "400", description = "Invalid id supplied", 
+			    content = @Content), 
+			 @ApiResponse(responseCode = "404", description = "Book not found", 
+			    content = @Content)})
 	@GetMapping(value = "/v1/github/{owner}/{repo}")
-	public List<Contributor> getGitHubContributors(@PathVariable String owner, @PathVariable String repo) {
-		return gitHubFeignService.getContributor(owner, repo);
+	public ResponseEntity<List<Contributor>> getGitHubContributors(
+			@Parameter(description = "owner to be searched") @PathVariable String owner,
+			@Parameter(description = "repository to be searched") @PathVariable String repo) {
+		try {
+			log.info("ResponseEntity<");
+			return ResponseEntity.ok().body(gitHubFeignService.getContributor(owner, repo));
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		// return gitHubFeignService.getContributor(owner, repo);
 	}
 }
--- src/main/java/com/abl/mydata/dto/Contributor.java
+++ src/main/java/com/abl/mydata/dto/Contributor.java
@@ -1,25 +1,45 @@
 package com.abl.mydata.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Github Contributor정보")
 public class Contributor {
+	@Schema(description = "로그인ID", nullable = false)
     String login;
+	@Schema(description = "ID", nullable = false)
     String id;
+	@Schema(description = "node_id", nullable = false)
     String node_id;
+	@Schema(description = "avatar_url", nullable = false)
     String avatar_url;
+	@Schema(description = "gravatar_id", nullable = false)
     String gravatar_id;
+	@Schema(description = "url", nullable = false)
     String url;
+	@Schema(description = "html_url", nullable = false)
     String html_url;
+	@Schema(description = "followers_url", nullable = false)
     String followers_url;
+	@Schema(description = "following_url", nullable = false)
     String following_url;
+	@Schema(description = "gists_url", nullable = false)
     String gists_url;
+	@Schema(description = "starred_url", nullable = false)
     String starred_url;
+	@Schema(description = "subscriptions_url", nullable = false)
     String subscriptions_url;
+	@Schema(description = "organizations_url", nullable = false)
     String organizations_url;
     String repos_url;
+    @Schema(description = "events_url", nullable = false)
     String events_url;
+    @Schema(description = "received_events_url", nullable = false)
     String received_events_url;
+    @Schema(description = "type", nullable = false)
     String type;
+    @Schema(description = "site_admin", nullable = false)
     String site_admin;
+    @Schema(description = "contributions", nullable = false)
     int contributions;
 }
